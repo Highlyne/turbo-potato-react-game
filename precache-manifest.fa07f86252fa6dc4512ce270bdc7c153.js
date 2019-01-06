527818d66bd47e208ba888965c279a779bc851e7
Tamara Hall <thall2000@gmail.com>
2019-01-06 09:20:08 -0500

    Updates

---
+++ precache-manifest.fa07f86252fa6dc4512ce270bdc7c153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c3ad40b2ea1c1ca4a4",
+    "url": "/turbo-potato-react-game/static/js/runtime~main.e8c3ad40.js"
+  },
+  {
+    "revision": "33cea821003752a7cb77",
+    "url": "/turbo-potato-react-game/static/js/main.33cea821.chunk.js"
+  },
+  {
+    "revision": "b228321dd8a2c6cba185",
+    "url": "/turbo-potato-react-game/static/js/1.b228321d.chunk.js"
+  },
+  {
+    "revision": "33cea821003752a7cb77",
+    "url": "/turbo-potato-react-game/static/css/main.8165a45d.chunk.css"
+  },
+  {
+    "revision": "b228321dd8a2c6cba185",
+    "url": "/turbo-potato-react-game/static/css/1.334ebb10.chunk.css"
+  },
+  {
+    "revision": "b7769dd8a3741c83e3c56d26fce8cf8b",
+    "url": "/turbo-potato-react-game/index.html"
+  }
+];
