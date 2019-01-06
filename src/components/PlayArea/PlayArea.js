025979c2e83a5a1f6f1cf2457f8f8e3c46d6803f
Tamara Hall <thall2000@gmail.com>
2019-01-06 10:01:46 -0500

    working on displaying images

--- src/components/PlayArea/PlayArea.js
+++ src/components/PlayArea/PlayArea.js
@@ -10,10 +10,10 @@ class PlayArea extends React.Component {
         return (
             <div className="playArea">
         
-                <Playbox boxAmount={this.props.gameState.box1amount} clickSum={this.props.clickSum} paddle="/images/pad1.png" />
-                <Playbox boxAmount={this.props.gameState.box2amount} clickSum={this.props.clickSum} paddle="/images/pad2.png"/>
-                <Playbox boxAmount={this.props.gameState.box3amount} clickSum={this.props.clickSum} paddle="/images/pad3.png"/>
-                <Playbox boxAmount={this.props.gameState.box4amount} clickSum={this.props.clickSum} paddle="/images/pad4.png"/>
+                <Playbox boxAmount={this.props.gameState.box1amount} clickSum={this.props.clickSum} paddle="./images/pad1.png" />
+                <Playbox boxAmount={this.props.gameState.box2amount} clickSum={this.props.clickSum} paddle="./images/pad2.png"/>
+                <Playbox boxAmount={this.props.gameState.box3amount} clickSum={this.props.clickSum} paddle="./images/pad3.png"/>
+                <Playbox boxAmount={this.props.gameState.box4amount} clickSum={this.props.clickSum} paddle="./images/pad4.png"/>
             </div>
         )
     }
