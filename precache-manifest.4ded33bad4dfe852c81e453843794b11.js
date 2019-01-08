37f6b893fa5521a6954992bc52983c0c56c5c9db
Tamara Hall <thall2000@gmail.com>
2019-01-07 22:16:46 -0500

    Updates

--- precache-manifest.4ded33bad4dfe852c81e453843794b11.js
+++ precache-manifest.4ded33bad4dfe852c81e453843794b11.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/turbo-potato-react-game/static/js/runtime~main.e8c3ad40.js"
   },
   {
-    "revision": "1ee841575acfa8019a93",
-    "url": "/turbo-potato-react-game/static/js/main.1ee84157.chunk.js"
+    "revision": "210cb516bf5a58860b37",
+    "url": "/turbo-potato-react-game/static/js/main.210cb516.chunk.js"
   },
   {
     "revision": "086e6aad35b2eef38eeb",
     "url": "/turbo-potato-react-game/static/js/1.086e6aad.chunk.js"
   },
   {
-    "revision": "1ee841575acfa8019a93",
-    "url": "/turbo-potato-react-game/static/css/main.b3023c01.chunk.css"
+    "revision": "210cb516bf5a58860b37",
+    "url": "/turbo-potato-react-game/static/css/main.6f710094.chunk.css"
   },
   {
     "revision": "086e6aad35b2eef38eeb",
     "url": "/turbo-potato-react-game/static/css/1.2ddc6f04.chunk.css"
   },
   {
-    "revision": "a008877bb29fa397786210b5add113d2",
+    "revision": "fedea57c3c3e973a2ccc07d745c9245e",
     "url": "/turbo-potato-react-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/turbo-potato-react-game/precache-manifest.fa68e2d7b5c6c2bf0e88d69ecc47d294.js"
+  "/turbo-potato-react-game/precache-manifest.4ded33bad4dfe852c81e453843794b11.js"
 );
 
 workbox.clientsClaim();
